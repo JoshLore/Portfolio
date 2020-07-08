4ab977e01b679bbc1091ccf69c381785545ce68d
Joshua Kelley <loredevelops@gmail.com>
2020-07-08 16:39:59 -0600

    Updates

---
+++ precache-manifest.a41c9c77ff41db822dda0f19111e55a5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5110f4a4d20a168e64bdb9f6073ae6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "98b1dbdd8e21ae5e3ef7",
+    "url": "/Portfolio/static/css/main.c00af3e5.chunk.css"
+  },
+  {
+    "revision": "8c28a192a3f5d50a9bf5",
+    "url": "/Portfolio/static/js/2.a1d538a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.a1d538a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b1dbdd8e21ae5e3ef7",
+    "url": "/Portfolio/static/js/main.cd530dba.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "118c8c9c1621e63f86b3c093a9abe2dc",
+    "url": "/Portfolio/static/media/HueTaskr.118c8c9c.png"
+  },
+  {
+    "revision": "47893cd2b0f5608a118317a7c8e7adec",
+    "url": "/Portfolio/static/media/Portfolio.47893cd2.png"
+  },
+  {
+    "revision": "ec29c0df426c1c320d48c6f06445cd35",
+    "url": "/Portfolio/static/media/The Coding Network.ec29c0df.png"
+  },
+  {
+    "revision": "422998298881e105f3b7636d0bb248d6",
+    "url": "/Portfolio/static/media/profile.42299829.png"
+  },
+  {
+    "revision": "e98f0c0cbd4dd40cc9fdc4335cafadec",
+    "url": "/Portfolio/static/media/video.e98f0c0c.mp4"
+  }
+]);
